fdc53e8946d9f6ea15526ecde210bfe51e4ca8d3
castrogusttavo <gusttavoleandro2023@gmail.com>
2024-10-09 14:33:06 -0300

    style: alter bg color input in light mode

--- components/docs/Header.tsx
+++ components/docs/Header.tsx
@@ -194,7 +194,7 @@ export function Header() {
                 <input
                   type="text"
                   placeholder="Search documentation"
-                  className="block w-full appearance-none rounded-lg px-3 py-2 transition-colors text-base leading-tight md:text-sm bg-slate-50/[0.5] dark:bg-grey-50/10 focus:bg-slate-50 dark:focus:bg-grey-50/5 focus:outline-none focus:border-none focus:ring-2 focus:ring-ring focus:ring-offset-background focus:ring-softis-light dark:focus:ring-softis-dark placeholder:text-grey-400 dark:placeholder:text-grey-200 text-grey-400 dark:text-grey-200"
+                  className="block w-full appearance-none rounded-lg px-3 py-2 transition-colors text-base leading-tight md:text-sm bg-slate-100 dark:bg-grey-50/10 focus:bg-slate-100/50 dark:focus:bg-grey-50/5 focus:outline-none focus:border-none focus:ring-2 focus:ring-ring focus:ring-offset-background focus:ring-softis-light dark:focus:ring-softis-dark placeholder:text-grey-400 dark:placeholder:text-grey-200 text-grey-400 dark:text-grey-200"
                   value={dataSearch}
                   onChange={(event) => setDataSearch(event.target.value)}
                 />
